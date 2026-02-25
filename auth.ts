c5864b486110424e693a04ef28299ce6ef96bb84
Test User <test@test.com>
2026-02-25 18:55:46 +0300

    Добавлен trustHost для Vercel

--- auth.ts
+++ auth.ts
@@ -8,6 +8,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true, // нужен для Vercel и прокси, чтобы редиректы работали
   adapter: PrismaAdapter(prisma),
   providers: [
     Google({
